d48d5bbc22c748715b3d8b92ca48461420c1366c
Lapo0 <lapo.gini.01@gmail.com>
2023-04-13 17:26:33 +0200

    FIX error

--- database/seeders/TrainsSeeder.php
+++ database/seeders/TrainsSeeder.php
@@ -17,6 +17,9 @@ class TrainsSeeder extends Seeder
     public function run(Faker $faker)
     {
 
+        $departure_time = $faker->dateTimeInInterval('-1 week', '+3 days');
+        $arrival_time = 
+
         for($i = 0; $i < 100; $i++) {
 
             $train = new Train();
@@ -24,12 +27,13 @@ class TrainsSeeder extends Seeder
             $train->agency = $faker->word();
             $train->departure_station = $faker->sentence(3);
             $train->arrival_station = $faker->sentence(3);
-            $train->departure_time = $faker->dateTimeInInterval('-1 week', '+3 days');
+            $train->departure_time = $departure_time;
             $train->arrival_time = $faker->dateTimeInInterval('-1 week', '+3 days');
-            $train->train_code = $faker->bothify('??-####');
+            $train->train_code = $faker->bothify('??-###');
             $train->carriages_num = $faker->numberBetween(0, 25);
             $train->is_on_time = $faker->boolean();
             $train->is_cancelled = $faker->boolean();
+            $train->price = $faker->numberBetween(12, 25);
 
             $train->save();
         }
